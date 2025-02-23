eaffd30d390002726ebd8e8b91518bb06a232291
jkchuong <jkchuong@gmail.com>
2025-02-23 19:28:31 +0100

    Added projectile spell and firebolt effect and blueprint

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,21 @@
+// Copyright JKChuong
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+};
