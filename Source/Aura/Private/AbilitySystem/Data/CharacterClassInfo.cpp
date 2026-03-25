f30083b9093c730c2c68a12a2da2395989fdff41
jkchuong <jkchuong@gmail.com>
2026-03-25 22:23:27 +0100

    Added data class for enemy character classes

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright JKChuong
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+#include "AuraLogs.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(const FGameplayTag CharacterClass)
+{
+	if (FCharacterClassDefaultInfo* Info = CharacterClassInformation.Find(CharacterClass))
+	{
+		return *Info;
+	}
+	
+	UE_LOG(LogAura, Error, TEXT("Could not find Character Class [%s] in Character Class DataAsset [%s]"), *CharacterClass.ToString(), *GetPathName());
+	return FCharacterClassDefaultInfo();
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,40 @@
+﻿// Copyright JKChuong
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+/**
+ *  Data containing information specific to character classes
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defaults", meta=(Categories="Character.Class"))
+	TMap<FGameplayTag, FCharacterClassDefaultInfo> CharacterClassInformation;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+	
+	FCharacterClassDefaultInfo GetClassDefaultInfo(const FGameplayTag CharacterClass);
+};
