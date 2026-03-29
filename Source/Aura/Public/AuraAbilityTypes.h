e157c639f9cc17b7e8895ac2b7b144fdf089a1a6
jkchuong <jkchuong@gmail.com>
2026-03-29 16:45:40 +0200

    Added duplicate override and struct ops type traits on custom context

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -6,6 +6,9 @@
 #include "GameplayEffect.h"
 #include "AuraAbilityTypes.generated.h"
 
+/**
+ *  Custom Gameplay Effect Context struct for adding in extra properties that we want tracked when being replicated
+ */
 USTRUCT(BlueprintType)
 struct FAuraGameplayEffectContext : public FGameplayEffectContext
 {
@@ -19,7 +22,20 @@ struct FAuraGameplayEffectContext : public FGameplayEffectContext
 	
 	virtual UScriptStruct* GetScriptStruct() const override
 	{
-		return FGameplayEffectContext::GetScriptStruct();
+		return GetScriptStruct();
+	}
+	
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FAuraGameplayEffectContext* Duplicate() const override
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
 	}
 	
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
@@ -31,4 +47,14 @@ protected:
 
 	UPROPERTY()
 	bool bIsBlockedHit = false;
+};
+
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true,
+	};
 };
